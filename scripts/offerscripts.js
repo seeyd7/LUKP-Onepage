0713500ebc5d69af42ecd46307c0ff40de6cf185
seeyd7 <67929947+seeyd7@users.noreply.github.com>
2023-10-10 15:13:12 +0200

    Poprawki RWD i offer

--- scripts/offerscripts.js
+++ scripts/offerscripts.js
@@ -1,3 +1,32 @@
+// Menu script
+const menuToggler = document.querySelector(".menu-toggler");
+const navMenu = document.querySelector(".navbar-menu");
+const darkenElement = document.querySelector(".darken");
+const navLinks = document.querySelectorAll(".navbar-menu a");
+
+menuToggler.addEventListener("click", () => {
+  if (menuToggler.classList.contains("active")) {
+    menuToggler.classList.remove("active");
+    navMenu.classList.remove("active");
+    darkenElement.style.zIndex = "-999";
+    darkenElement.style.opacity = "0";
+  } else {
+    menuToggler.classList.add("active");
+    navMenu.classList.add("active");
+    darkenElement.style.zIndex = "2";
+    darkenElement.style.opacity = "0.5";
+  }
+});
+
+// navLinks.forEach(link => {
+//   link.addEventListener("click", () => {
+//     menuToggler.classList.remove("active");
+//     navMenu.classList.remove("active");
+//     darkenElement.style.zIndex = "-999";
+//     darkenElement.style.opacity = "0";
+//   });
+// });
+
 window.onscroll = () => { 
     const myNav = document.querySelector("nav");
 
@@ -14,74 +43,6 @@ window.onscroll = () => {
     }
 };
 
-window.onload = () => { 
-    const myNav = document.querySelector("nav");
-
-    if (window.scrollY > 50) {
-        myNav.classList.add("navbar-scrolled");
-        myNav.classList.remove("navbar-top");
-    } else {
-        myNav.classList.remove("navbar-scrolled");
-        myNav.classList.add("navbar-top");
-    }
-};
-
-const menu = document.querySelector("#menu");
-
-const navMenu = document.querySelector(".navbar-menu");
-const menuToggler = document.querySelector(".menu-toggler");
-
-const navbar = document.querySelector("nav");
-
-
-menuToggler.addEventListener("click", () => {
-	if(menuToggler.classList.contains("active")) {
-		menuToggler.classList.remove("active");
-		navMenu.classList.remove("active");
-	} else {
-		menuToggler.classList.add("active");
-		navMenu.classList.add("active");
-        sliders.forEach((slider, i) => {
-            slider.classList.contains("active2") ? slider.classList.remove("active2", i == this.id)
-                                                 : slider.classList.toggle("active2", i == this.id);
-        });
-	}
-});
-
-function disableMenu() {
-    menuToggler.classList.remove("active");
-    navMenu.classList.remove("active");
-}
-
-addEventListener("scroll", () => {
-	let scroll = this.scrollY;
-	if(scroll > menu.clientHeight) {
-    go2Top.style.opacity = 1;
-    go2Top.style.lineHeight = "40px";
-		go2Top.style.cursor = "pointer";
-	} else {
-		go2Top.style.opacity = 0;
-		go2Top.style.cursor = "default";
-	}
-})
-
-go2Top.addEventListener("click", () => {
-	if(go2Top.style.opacity != 0) window.scrollTo(0, 0);
-})
-
-const navBar = document.querySelector(".navbar-top");
-const topBar = document.querySelector(".top-bar");
-
-addEventListener("scroll", () => {
-    let scroll = this.scrollY;
-    if(scroll > navBar.clientHeight/2) {
-        topBar.style.top = "-40px";
-        navBar.style.top = 0;
-    } else {
-        topBar.style.top = 0;
-        navBar.style.top = "38px";
-    }
-})
 
 function myhref(web) {
     window.open('https://www.google.com/maps/dir//Lud%C5%BAmierska+25A+34-400+Nowy+Targ/@49.4728089,20.0094221,17z/data=!4m5!4m4!1m0!1m2!1m1!1s0x4715e4fc2204c859:0x99fb257976a16172', '_blank');
@@ -133,3 +94,28 @@ for (var i = 0; i < offerSliderQuestion.length; i++) {
        wybrany_tekst.style.display = "block";
    });
 }
+
+// Footer animation
+function animateSectionsOnScroll(entries, observer) {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        const section = entry.target;
+  
+        section.style.opacity = 1;
+        section.classList.add('animate__animated', 'animate__fadeIn');
+  
+        observer.unobserve(section);
+      }
+    });
+  }
+  
+  const sections = document.querySelectorAll('.triple > div, .fb-div');
+  const observerOptions = {
+    threshold: 0.2
+  };
+  const sectionObserver = new IntersectionObserver(animateSectionsOnScroll, observerOptions);
+  
+  sections.forEach(section => {
+    section.style.opacity = 0;
+    sectionObserver.observe(section);
+  });
